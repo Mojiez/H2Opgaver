f7f2400a31480db384c59c9ab6c851a6216fa0bd
Kean <50844146+Mojiez@users.noreply.github.com>
2020-11-19 10:32:38 +0100

    Added ExtraEquipment class

---
+++ Hotel_LandLyst/Hotel_LandLyst_WebApp/Models/ExtraEquipment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_LandLyst_WebApp.Models
+{
+    public class ExtraEquipment
+    {
+        public bool Balconi { get; set; }
+        public bool DoubleBed { get; set; }
+        public bool Tub { get; set; }
+        public bool Jacuzzi { get; set; }
+        public bool Kitchen { get; set; }
+
+        public ExtraEquipment(bool balconi, bool doubleBed, bool tub, bool jacuzzi, bool kitchen)
+        {
+            Balconi = balconi;
+            DoubleBed = doubleBed;
+            Tub = tub;
+            Jacuzzi = jacuzzi;
+            Kitchen = kitchen;
+        }
+    }
+}
