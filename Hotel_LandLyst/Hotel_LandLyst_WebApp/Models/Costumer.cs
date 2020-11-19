42c1dbf66b4a206735652f16395229d581bba7fd
Kean <50844146+Mojiez@users.noreply.github.com>
2020-11-19 10:27:53 +0100

    Added Costumer class

---
+++ Hotel_LandLyst/Hotel_LandLyst_WebApp/Models/Costumer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_LandLyst_WebApp.Models
+{
+    public class Costumer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Costumer(int id, string name, string address, string postalCode, int phoneNumber, string email)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            PostalCode = postalCode;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
