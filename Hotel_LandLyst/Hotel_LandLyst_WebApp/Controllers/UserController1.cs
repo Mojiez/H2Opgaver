a6a7940007b0724e0328228e97fc7eec336e12f2
Kenneth Andersen (KENN229K) <kenn229k@zbc.dk>
2020-11-19 17:27:33 +0100

    Added controllers for user and admin, added documentation

---
+++ Hotel_LandLyst/Hotel_LandLyst_WebApp/Controllers/UserController1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_LandLyst_WebApp.Controllers
+{
+    public class UserController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
