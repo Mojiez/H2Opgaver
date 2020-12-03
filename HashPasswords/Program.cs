7ceeadc09128ec1d61e9d2c20862d754b89ae304
Kenneth Andersen (KENN229K) <kenn229k@zbc.dk>
2020-12-03 22:22:58 +0100

    update

--- HashPasswords/Program.cs
+++ HashPasswords/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using App_Dal;
 
 namespace CryptographyInDotNet
 {
@@ -7,25 +8,30 @@ namespace CryptographyInDotNet
     {
         static void Main()
         {
-            const string password = "V3ryC0mpl3xP455w0rd";
-            byte[] salt = Hash.GenerateSalt();
+            //const string password = "V3ryC0mpl3xP455w0rd";
+            //byte[] salt = Hash.GenerateSalt();
 
-            Console.WriteLine("Hash Password with Salt Demonstration in .NET");
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine();
-            Console.WriteLine("Password : " + password);
-            Console.WriteLine("Salt = " + Convert.ToBase64String(salt));
-            Console.WriteLine();
+            //Console.WriteLine("Hash Password with Salt Demonstration in .NET");
+            //Console.WriteLine("---------------------------------------------");
+            //Console.WriteLine();
+            //Console.WriteLine("Password : " + password);
+            //Console.WriteLine("Salt = " + Convert.ToBase64String(salt));
+            //Console.WriteLine();
 
-            var hashedPassword1 = Hash.HashPasswordWithSalt(
-                Encoding.UTF8.GetBytes(password),
-                salt);
+            //var hashedPassword1 = Hash.HashPasswordWithSalt(
+            //    Encoding.UTF8.GetBytes(password),
+            //    salt);
 
-            Console.WriteLine();
-            Console.WriteLine("Hashed Password = " + Convert.ToBase64String(hashedPassword1));
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Hashed Password = " + Convert.ToBase64String(hashedPassword1));
+            //Console.WriteLine();
 
-            Console.ReadLine();
+            //Console.ReadLine();
+
+            string input = "hello";
+            
+            Console.WriteLine($"Input: {input} {"Begins with uppercase? ", 30}: " +
+                              $"{(input.StartsWithUpper() ? "Yes" : "No")}\n");
         }
     }
 }
